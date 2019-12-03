464d7257eeb687f46ebed9921be05fe57f242f44
J-E Castagnede <j.e.castagnede@gmail.com>
2019-12-03 13:47:43 +0100

    improve setup

--- setup.py
+++ setup.py
@@ -34,6 +34,9 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Framework :: Django',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
     ],
     install_requires=[
         'django>=2.2',
