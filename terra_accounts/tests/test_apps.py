b53fab1e8102830591760ecb959d86c4351ef22e
J-E Castagnede <j.e.castagnede@gmail.com>
2020-06-02 16:55:50 +0200

    fix tests

--- terra_accounts/tests/test_apps.py
+++ terra_accounts/tests/test_apps.py
@@ -7,14 +7,14 @@ from rest_framework.test import APITestCase
 class AppTestCase(APITestCase):
     @override_settings(JWT_AUTH={'JWT_EXPIRATION_DELTA': 9999})
     def test_settings_patched(self):
-        response = self.client.get(reverse('terra_utils:settings'))
+        response = self.client.get(reverse('terra_accounts:settings'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         data = response.json()
         self.assertEqual(data['jwt_delta'], 9999)
 
     def test_settings_default(self):
-        response = self.client.get(reverse('terra_utils:settings'))
+        response = self.client.get(reverse('terra_accounts:settings'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
         data = response.json()
