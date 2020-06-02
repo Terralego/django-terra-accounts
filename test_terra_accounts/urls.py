18a8a4df16627ca67d1759d253ac9e0a3bf538f5
J-E Castagnede <j.e.castagnede@gmail.com>
2020-06-02 16:42:59 +0200

    dont include terra_utils urls anymore

--- test_terra_accounts/urls.py
+++ test_terra_accounts/urls.py
@@ -18,6 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('terra_utils.urls')),
     path('api/', include('terra_accounts.urls')),
 ]
