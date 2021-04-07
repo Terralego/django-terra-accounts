eb11aad80e1053a08ae98b90e25220fc28cc0ea5
vllorca <vivien.llorca@makina-corpus.com>
2021-04-07 15:33:53 +0200

    Add manager to filter permission with disabled_modules setting

--- terra_accounts/signals.py
+++ terra_accounts/signals.py
@@ -1,16 +1,19 @@
 import logging
+from django.conf import settings
 
 
 logger = logging.getLogger(__name__)
 
 
 def permission_callback(sender, **kwargs):
-    logger.info(f'Updating permissions of {sender.name}')
+    logger.info(f"Updating permissions of {sender.name}")
 
-    ContentType = sender.apps.get_model('contenttypes.ContentType')
-    TerraPermission = sender.apps.get_model('terra_accounts.TerraPermission')
+    ContentType = sender.apps.get_model("contenttypes.ContentType")
+    TerraPermission = sender.apps.get_model("terra_accounts.TerraPermission")
 
     for module, perm, name in sender.permissions:
+        if module in settings.TERRA_APPLIANCE_SETTINGS["disabled_modules"]:
+            continue
 
         ctype = ContentType.objects.get_for_model(TerraPermission)
 
@@ -18,7 +21,7 @@ def permission_callback(sender, **kwargs):
             content_type=ctype,
             codename=perm,
             defaults={
-                'name': name,
-                'module': module
+                "name": name,
+                "module": module
             }
         )
